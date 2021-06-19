1f815a7d31c96994863cf50400d92e23cf8a8a10
mather <misaka@misaka.im>
2021-06-19 15:04:35 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c54c0f49a199739110ce4ea2a33f3702'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'afeeeff8-99ff-449e-a799-a251c0de5749'
+};
